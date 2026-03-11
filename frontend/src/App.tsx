087240a809ffc3e37232b24145f27ec5ec70add9
kohkakohla <ivanjerrickkoh@gmail.com>
2026-03-12 04:41:20 +0800

    Add admin upgrade / OTP generation pages to dashboard, fix JWT not being properly saved in localStorage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,40 +2,57 @@ import Login from "./pages/Login.tsx";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { JSX } from "react";
 import Register from "./pages/Register.tsx";
-
-function Home() {
-    return <h1>Welcome to PeerPrep 🎉</h1>;
-}
+import AdminUpgrade from "./pages/AdminUpgrade.tsx";
+import GenerateOTP from "./pages/GenerateOTP.tsx";
+import { Home } from "./pages/Home.tsx";
 
 function ProtectedRoute({ children }: { children: JSX.Element }) {
-    const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-    if (!token) {
-        return <Navigate to="/login" replace />;
-    }
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
 
-    return children;
+  return children;
 }
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
 
-                <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Register />} />
 
-                <Route
-                    path="/"
-                    element={
-                        <ProtectedRoute>
-                            <Home />
-                        </ProtectedRoute>
-                    }
-                />
+        <Route
+          path="/admin-upgrade"
+          element={
+            <ProtectedRoute>
+              <AdminUpgrade />
+            </ProtectedRoute>
+          }
+        />
 
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-        </BrowserRouter>
-    );
+        <Route
+          path="/generate-otp"
+          element={
+            <ProtectedRoute>
+              <GenerateOTP />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
